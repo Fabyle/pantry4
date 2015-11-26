710168fbba48438ece56c904e70f6cfb306bf2fa
fabien <fabien99@gmail.com>
2015-11-26 18:48:11 +0100

    finalisation de la lecture des plugins

--- RubyProject1/AnalysePlugIn.rb
+++ RubyProject1/AnalysePlugIn.rb
@@ -3,19 +3,23 @@
 require 'rubygems'
 require 'zip'
 
-path = 'C:\Users\fabien\git\RubyProject1\plugins'
+path = ARGV[0]
+#path = 'C:\Users\fabien\git\RubyProject1\plugins'
 name = ""
 version = ""
 h = Hash.new();
 
-
-
-
+#---
+# Recherche des fichiers dans le répertoires
+#---
 Dir.foreach(path) do |item|
+  # Exclusion et prise en compte que des systalians
   next if item == '.' or item == '..' or !item.include? "systalians"
+  # ouverture des jars - en tant que zip
   Zip::File.open(path+'\\'+item) do |zip_file|
-    # Handle entries one by one
+    # Analyse de chaque element dans le zip
     zip_file.each do |entry|
+      # On ne prend que la MANIFEST
       if ((entry.file?) && (entry.name == "META-INF/MANIFEST.MF"))
         content = entry.get_input_stream.read
         content.each_line do  |line|
@@ -27,6 +31,7 @@ Dir.foreach(path) do |item|
             version=array[1]
           end  #if
         end #do
+        # on stocke dans h le nom et la version du plugin
         h[name]=version
       end #if
     end #do
@@ -34,5 +39,14 @@ Dir.foreach(path) do |item|
 end #do
 
 
+#---
+# Tri et restitution
+#---
+hsorted = h.sort.to_h
 
-puts h.sort.to_h
+hsorted.each do | key , value |
+  puts("#{key};#{value}") 
+end
+
+
+
